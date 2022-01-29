30ae7697a781c8ce94e07b1b42529821521932d2
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-01-29 19:32:47 +0530

    Done with layout and UI of the app

---
+++ app/src/main/java/com/example/todoapp/Adapter.kt
@@ -0,0 +1,25 @@
+package com.example.todoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter:RecyclerView.Adapter<Adapter.viewHolder>() {
+    class viewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        var itemView=LayoutInflater.from(parent.context).
+                inflate(R.layout.view,parent,false)
+
+        return viewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
